6f89aeb33c8db27b44c031b5702ba75ff5e0ab3e
Opilane TTHK <opilane@tthk.ee>
2020-10-21 16:02:35 +0300

    add cvv validation

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace IDCodeReader
             {
                 Console.WriteLine("Welcome");
                 HelloUser(usersID);
-                GetYear(usersID);
+                GetAge(usersID);
             }else
             {
                 Console.WriteLine("Wrong Format N00b. Try Again.");
@@ -52,8 +52,9 @@ namespace IDCodeReader
                 Console.WriteLine("Hello,Madam!");
             }
         } 
-        public static void GetYear(string idCode)
+        public static int GetYear(string idCode)
         {
+
             string yearFromCode = idCode.Substring(1, 2);
             string year;
             if(int.Parse(idCode[0].ToString()) > 4)
@@ -65,6 +66,18 @@ namespace IDCodeReader
                 year = "19" + yearFromCode;
             }
             Console.WriteLine($"You were born in {year}");
+            int yearParsed = Int32.Parse(year);
+
+            return yearParsed; 
+        }
+        public static void GetAge(string idCode)
+        {
+            int yearOfBirth = GetYear(idCode);
+
+            DateTime now = DateTime.Now;
+            int yearNow = Int32.Parse(now.Year.ToString());
+            int age = yearNow - yearOfBirth;
+            Console.WriteLine($"You are {age} years old.");
         }
             
         
